3c11ea9b929031de167169692d19c01ffe2cbefa
RebikHub <rebikovlk@mail.ru>
2024-05-27 16:45:47 +0200

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command !== 'serve' ? '/pwa-media-test/' : '/',
+  base: command !== 'serve' ? '/checkers/' : '/',
   plugins: [VitePWA({
     registerType: 'autoUpdate',
     // strategies: 'injectManifest',
